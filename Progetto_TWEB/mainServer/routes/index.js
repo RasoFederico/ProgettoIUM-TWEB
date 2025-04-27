ccce1efb654c96a0190d265ff7a3cb55286a9065
Federico Raso <federico.raso@edu.unito.it>
2025-04-27 19:53:26 +0200

    iniziato a lavorare sulla comunicazione tra il server principale e quello per il db in postgres

--- Progetto_TWEB/mainServer/routes/index.js
+++ Progetto_TWEB/mainServer/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Filmoteca' });
 });
+
+router.post('/get-movie-by-name', function(req, res, next) {
+  const normalized = {...req.body};
+  console.log(normalized);
+  res.send("mandato tutto correttamente");
+})
 
 module.exports = router;
